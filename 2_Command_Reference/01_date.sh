68af6b8c08df6b6146e7e2373c7dee7995820603
Mohit Kumar <mohitkr05@gmail.com>
2022-05-21 10:22:26 +0000

    dates

--- 2_Command_Reference/01_date.sh
+++ 2_Command_Reference/01_date.sh
@@ -21,8 +21,53 @@ date +"Year: %Y"
 date +"%a"
 date +"%A"
 
-# Print the weekday , 'a' for first three letters and 'A' for full day 
+# Print the Month , 'b' or 'h' for abbrevated month name and 'B' for full month name
 
+date +"%b"
+date +"%h"
+date +"%B"
+
+# Print the date in locale format - %c
+date +"%c"
+
+# Print the centure - %C 
+date +"%C"
+
+# Print the date in number  - %d 
+date +"%d"
+
+# Print the date in the format equivalent to %m/%d/%y.
+date +"%D"
+
+# Print the day of the month as a decimal number 
+date +"%e"
+
+# Print the hour as a 24 format decimal number 
+date +"%H"
+
+# Print the hour as a 12 hour format 
+date +"%I"
+
+# Print the clock as 24 hour format 
+date +"%T"
+
+# Print the day of the year in decimal number 
+date +"%j"
+
+# Print the month of year as decimal number 
+date +"%m"
+
+# Print the minutes 
+date +"%M"
+
+# Print the AM / PM 
+date +"%p"
+
+# Print the clock with AM/PM 
+date + "%r"
+
+# Print the seconds 
+date + "%s"
 
 
 # let us see how to change the date
